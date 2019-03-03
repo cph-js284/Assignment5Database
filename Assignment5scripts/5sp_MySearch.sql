c39ec2975a62f6b6e9bc7b56b7f412426c633e02
Jacob <cph-js284@cphbusiness.dk>
2019-03-03 11:01:49 +0100

    Refactored MySearch SP

--- Assignment5scripts/5sp_MySearch.sql
+++ Assignment5scripts/5sp_MySearch.sql
@@ -5,18 +5,17 @@ DELIMITER $$
 USE `stackoverflow`$$
 CREATE DEFINER=`root`@`%` PROCEDURE `MySearch`(p_keyword text)
 BEGIN
-select *
-from (
-SELECT *
+select JsonQandA.postId, commentsData.KWCounter
+from 
+(SELECT postId
 from matView
-where JSON_EXTRACT(QandA, "$.Question", '$.Answer') like concat('%', p_keyword, '%')
-) as JsonQandA
-left join (
-SELECT PostId, count(*) as KWCounter 
+where JSON_EXTRACT(QandA, "$.Question", '$.Answer') like concat('%', p_keyword, '%')) as JsonQandA
+inner join 
+(SELECT PostId, count(*) as KWCounter 
 FROM comments 
 where Text like concat('%', p_keyword, '%')
-group by PostId) as commentsData on commentsData.PostId = JsonQandA.postId
-where commentsData.KWCounter > 1;
+group by PostId Having KWCounter > 1) 
+as commentsData on commentsData.PostId = JsonQandA.postId;
 END$$
 
 DELIMITER ;
